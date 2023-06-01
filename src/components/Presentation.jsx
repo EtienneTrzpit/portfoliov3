bd16b26e61edaa6ad67855e97d7354152689db6e
EtienneTrzpit <etienne.trzpit@free.fr>
2023-06-01 11:16:52 +0200

    presentation

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -1,9 +1,13 @@
+import photo from '../assets/photo-personnelle.png';
 
 export default function Presentation() {
     return (
         <article className="presentation">
-            <h1>Etienne Trzpit</h1>
-            <p>Intégrateur Web avec développement en React</p>
+            <div className='presentation-txt'>
+                <h1 className="title">Etienne Trzpit</h1>
+                <p className="subtitle">Intégrateur Web en développement en React</p>
+            </div>
+            <img className="presentation-img" src={photo} alt="Photo personnelle"/>
         </article>
     )
 }
