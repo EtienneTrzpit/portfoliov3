45984c33067c40636234c163804a35c189aca96e
EtienneTrzpit <etienne.trzpit@free.fr>
2023-06-03 12:04:20 +0200

    header smartphone

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,8 @@ import { faBars } from '@fortawesome/free-solid-svg-icons'
 export default function Header() {
     return(
         <header className='header-top'>
-            <p>ETIENNE TRZPIT</p>
-            <FontAwesomeIcon icon={faBars} />
+            <p className='name'>ETIENNE TRZPIT</p>
+            <FontAwesomeIcon icon={faBars} size="4x" className='hamburger'/>
         </header>
     )
 }
