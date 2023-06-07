497707f75f8d41e4dcac6eb3d6f5b262145ff3bb
EtienneTrzpit <etienne.trzpit@free.fr>
2023-06-07 10:39:49 +0200

    version1 savoir-faire

--- src/components/SavoirFaire.jsx
+++ src/components/SavoirFaire.jsx
@@ -13,23 +13,25 @@ export default function SavoirFaire() {
             <FontAwesomeIcon icon={faBrain} size='2xl'/>
             <h2 className='cards-header-title'>Savoir-Faire</h2>
             </div>
-            <div className='cards-columns column-knowledge'>
-                <h3 className='cards-columns-title title-knowledge'>Front-End</h3>
-                <ul>
-                    <li><FontAwesomeIcon icon={faHtml5} size='xl' className='knowledge-icons'/>HTML5 Sémantique</li>
-                    <li><FontAwesomeIcon icon={faCss3Alt} size='xl' className='knowledge-icons'/>CSS3 Animation</li>
-                    <li><FontAwesomeIcon icon={faSquareJs} size='xl' className='knowledge-icons'/>Javascript</li>
-                    <li><FontAwesomeIcon icon={faSass} size='xl' className='knowledge-icons'/>Sass</li>
-                </ul>
-            </div>
-            <div className='cards-columns column-knowledge'>
-                <h3 className='cards-columns-title title-knowledge'>Déploiement</h3>
-                <ul>
-                    <li>Git</li>
-                    <li>GitHub</li>
-                    <li>VS Code</li>
-                    <li>Netlify</li>
-                </ul>
+            <div className='cards-main'>
+                <section className='cards-columns columns-knowledge'>
+                    <h3 className='cards-columns-title title-knowledge'>Front-End</h3>
+                    <ul>
+                        <li><FontAwesomeIcon icon={faHtml5} size='xl' className='knowledge-icons'/>HTML5 Sémantique</li>
+                        <li><FontAwesomeIcon icon={faCss3Alt} size='xl' className='knowledge-icons'/>CSS3 Animation</li>
+                        <li><FontAwesomeIcon icon={faSquareJs} size='xl' className='knowledge-icons'/>Javascript</li>
+                        <li><FontAwesomeIcon icon={faSass} size='xl' className='knowledge-icons'/>Sass</li>
+                    </ul>
+                </section>
+                <section className='cards-columns columns-knowledge'>
+                    <h3 className='cards-columns-title title-knowledge'>Déploiement</h3>
+                    <ul>
+                        <li>Git</li>
+                        <li>GitHub</li>
+                        <li>VS Code</li>
+                        <li>Netlify</li>
+                    </ul>
+                </section>
             </div>    
         </section>
         
