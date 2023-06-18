0316a9845e587fa8c2ff979e1c1df678fe17e254
EtienneTrzpit <etienne.trzpit@free.fr>
2023-06-18 19:26:47 +0200

    pdf react

--- src/components/CertificationsImg.jsx
+++ src/components/CertificationsImg.jsx
@@ -1,6 +1,8 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import '../index.css'
 import { faFileArrowDown } from '@fortawesome/free-solid-svg-icons'
+import sass from '../assets/certifications/sass.pdf'
+import ReactPDF from '@react-pdf/renderer';
 
 export default function CertificationsImg({image}) {
 
@@ -9,7 +11,9 @@ export default function CertificationsImg({image}) {
     return (
         <div className={numero} key={image.numero}>
             <div className='titles-projects'>
+                <a href={sass} download={sass}>
                 <FontAwesomeIcon icon={faFileArrowDown} size='2x' className='download'/>
+                </a>
                 <h3>
                 <FontAwesomeIcon icon={image.title} size='4x' className='name-certification' />
                 </h3>
