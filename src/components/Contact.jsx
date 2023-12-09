64a1a51b103fa6b4dbcd98a0de1b552ff69aaac3
Etienne Trzpit <etienne.trzpit@free.fr>
2023-12-09 14:07:16 +0100

    style form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,44 +1,8 @@
 import "../index.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
 
 export default function Contact(props) {
-  /* const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-
-  function validateEmail(email) {
-    var emailReg = new RegExp(
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/i
-    );
-    return emailReg.test(email);
-  }
-
-  function handleChange(e) {
-    switch (e.target.name) {
-      case "name":
-        setName(e.target.value);
-        break;
-      case "email":
-        setEmail(e.target.value);
-        break;
-      case "message":
-        setMessage(e.target.value);
-        break;
-      default:
-        break;
-    }
-  }
-  
-  function handleSubmit(e) {
-    if (validateEmail(email)) {
-      alert("Votre message a bien été envoyé");
-    } else {
-      alert("Veuillez entrer une adresse email valide");
-    }
-  }*/
-
   return (
     <section
       ref={(ref) => (props.cardRefs.current[3] = ref)}
@@ -54,12 +18,12 @@ export default function Contact(props) {
           <input type="hidden" name="form-name" value="contact" />
           <p>
             <label>
-              Your Name: <input type="text" name="name" />
+              Ton nom: <input type="text" name="name" />
             </label>
           </p>
           <p>
             <label>
-              Your Email: <input type="email" name="email" />
+              Ton Email: <input type="email" name="email" />
             </label>
           </p>
           <p>
@@ -68,7 +32,7 @@ export default function Contact(props) {
             </label>
           </p>
           <p>
-            <button type="submit">Send</button>
+            <button type="submit">Envoyer</button>
           </p>
         </form>
       </div>
