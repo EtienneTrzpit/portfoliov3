f25729658166b39f6f746b80235821005c3c6021
Etienne Trzpit <etienne.trzpit@free.fr>
2024-04-30 10:57:11 +0200

    project width + delete arrow menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,41 +1,14 @@
 import "../index.css";
-import { useEffect } from "react";
 import logo from "../assets/logo/logo2.svg";
 
 export default function NavBar() {
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollY = window.scrollY;
-      if (currentScrollY < 250) {
-        document.querySelector(".arrow-up")?.classList.remove("arrow-up");
-        document.querySelector(".item1 span").classList.add("arrow-up");
-      } else if (currentScrollY >= 250 && currentScrollY < 800) {
-        document.querySelector(".arrow-up")?.classList.remove("arrow-up");
-        document.querySelector(".item2 span").classList.add("arrow-up");
-      } else if (currentScrollY >= 800 && currentScrollY < 1300) {
-        document.querySelector(".arrow-up")?.classList.remove("arrow-up");
-        document.querySelector(".item3 span").classList.add("arrow-up");
-      } else if (currentScrollY >= 1300 && currentScrollY < 1900) {
-        document.querySelector(".arrow-up")?.classList.remove("arrow-up");
-        document.querySelector(".item4 span").classList.add("arrow-up");
-      } else if (currentScrollY >= 1900) {
-        document.querySelector(".arrow-up")?.classList.remove("arrow-up");
-        document.querySelector(".item5 span").classList.add("arrow-up");
-      }
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
     <nav className="navig--large">
       <img src={logo} alt="logo" className="logo-desktop" />
       <ul>
         <li className="item1">
           <a href="#">Présentation</a>
-          <span className="select-hidden arrow-up"></span>
+          <span className="select-hidden"></span>
         </li>
         <li className="item2">
           <a href="#savoir-faire">Compétences</a>
